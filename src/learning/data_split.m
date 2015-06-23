cacdcdc59b089ade7add95fbcf11a11733fce501
Peijie You <you_peijie@yahoo.com>
2015-06-23 23:14:36 +0800

    去除PCA第二维，效果提升
    plot结果表明第二维与speaker关系很大，应该是表征人身份的特征。

---
+++ src/learning/data_split.m
@@ -0,0 +1,9 @@
+function [train_label, test_label] = data_split(index)
+train_label = 1:2400;
+test_label = [];
+for i = 1:3200
+    if sum(train_label == i) == 0
+        test_label = [test_label, i];
+    end
+end
+end
--- src/test.m
+++ src/test.m
@@ -1,4 +1,3 @@
-
 %   mine
 addpath 'C:\Users\You\Documents\GitHub\SpeechR\src\feature';
 addpath 'C:\Users\You\Documents\GitHub\SpeechR\src\learning';
@@ -21,29 +20,26 @@ addpath 'C:\Users\You\Documents\GitHub\SpeechR\src\lib\libsvm\windows';
 %     pause(0.7);
 % end
 
-% [heart_scale_label, heart_scale_inst] = libsvmread('heart_scale');
-% model = libsvmtrain(heart_scale_label, heart_scale_inst, '-c 1 -g 0.07');
-% [predict_label, accuracy, dec_values] = libsvmpredict(heart_scale_label, heart_scale_inst, model);
 clear
 clc
 load('../data/feature.mat');
+load('../data/label.mat');
 tfeature = PCA(feature')';
 size(tfeature)
 result = [];
-for i = 10:10:200
-feature = tfeature(:, 1:i);
-load('../data/label.mat');
 index = randperm(size(label, 1));
-train_index = 1:2400;
-test_index = 2401:3200;
-% train_index = 1:2800;
-% test_index = 2800:3200;
-train_feature = feature(train_index, :);
-test_feature = feature(test_index, :);
-train_label = label(train_index, :);
-test_label = label(test_index, :);
-model = libsvmtrain(train_label * 1.0, train_feature, '-s 0 -c 0.1 -t 0');
-[predict_label, accuracy, dec_values] = libsvmpredict(test_label * 1.0, test_feature, model);
-result = [result, accuracy];
+[train_index, test_index] = data_split(index);
+% 3d data display
+for i = 10:5:100
+    feature = tfeature(:, 1:i);
+    %   drop the 2nd pc(person related)
+    train_feature = feature(train_index, [1, 3:end]);
+    test_feature =  feature(test_index, [1, 3:end]);
+    train_label = label(train_index, :);
+    test_label = label(test_index, :);
+    model = libsvmtrain(train_label * 1.0, train_feature, '-s 0 -c 0.01 -t 0 -m 64');
+    [predict_label, accuracy, dec_values] = libsvmpredict(test_label * 1.0, test_feature, model);
+    result = [result, accuracy];
 end
-plot(result(1, :));
+hold on;
+plot(10:5:100, result(1, :), 'b');
